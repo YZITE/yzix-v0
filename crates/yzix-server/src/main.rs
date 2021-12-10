e451a194ccbcb3cf9fe4e496d9ed8944a4462bd4
zseri <zseri.devel@ytrizja.de>
2021-12-10 17:01:37 +0100

    server: fix missing return after realisation caching hit

--- crates/yzix-server/src/main.rs
+++ crates/yzix-server/src/main.rs
@@ -195,6 +195,7 @@ async fn schedule(
                             })
                             .await
                             .unwrap();
+                        return;
                     }
                 }
             }
