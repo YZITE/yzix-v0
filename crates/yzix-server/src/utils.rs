e7dea1e39b4236b4feb9f691c72dbfe8dc01a55b
zseri <zseri.devel@ytrizja.de>
2021-12-11 01:37:35 +0100

    fix leftover from rebase

--- crates/yzix-server/src/utils.rs
+++ crates/yzix-server/src/utils.rs
@@ -34,7 +34,6 @@ pub fn push_response(
     n: &mut build_graph::Node<NodeMeta>,
     kind: ResponseKind,
 ) -> impl Future<Output = ()> + '_ {
-    println!("push_response: {:?} {:?}", n, kind);
     log_to_bunch(
         &mut n.rest.log,
         LogFwdMessage::Response(Arc::new(Response {
